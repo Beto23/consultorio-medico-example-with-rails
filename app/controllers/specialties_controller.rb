326955070a0dac21bf0f14727642571af611f2a7
Beto23 <josee-45@hotmail.com>
2016-04-17 14:49:23 -0500

    Add specialties.all

--- app/controllers/specialties_controller.rb
+++ app/controllers/specialties_controller.rb
@@ -1,4 +1,16 @@
 class SpecialtiesController < ApplicationController
+
+  #Para pasar datos a la vista desde el controller se utilizan variables de clase
+    ##Y estas variables empiezan con un @variable estas variables podremos tener acceso
+    ##tanto como en la vista y el controller. Las variables locales que no tienen @ solo
+    ## son accesibles para el controller.
+
+
+  #GET /specialties
   def index
+    #obtiene todas los registros SELECT * FROM specialties
+
+    @specialties = Specialty.all
   end
+
 end
