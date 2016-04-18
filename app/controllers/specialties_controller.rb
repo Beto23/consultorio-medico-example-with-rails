3b5f0b7b7993583630c7a8e1acf191ae0d252fb9
Beto23 <josee-45@hotmail.com>
2016-04-17 22:13:21 -0500

    Add update specialty

--- app/controllers/specialties_controller.rb
+++ app/controllers/specialties_controller.rb
@@ -1,5 +1,7 @@
 class SpecialtiesController < ApplicationController
 
+  before_action :set_specialty, except: [:index, :new, :create]
+
   #Para pasar datos a la vista desde el controller se utilizan variables de clase
     ##Y estas variables empiezan con un @variable estas variables podremos tener acceso
     ##tanto como en la vista y el controller. Las variables locales que no tienen @ solo
@@ -25,7 +27,19 @@ class SpecialtiesController < ApplicationController
     end
   end
 
+  def update
+  if @specialty.update(specialty_params)
+    redirect_to specialties_path
+  else
+    render :edit
+  end
+end
+
   private
+
+  def set_specialty
+    @specialty = Specialty.find(params[:id])
+  end
 
   def specialty_params
     params.require(:specialty).permit(:name)
