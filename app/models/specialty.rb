e0724cd821657646452ed109122bf108c10c9f95
Beto23 <josee-45@hotmail.com>
2016-04-17 23:41:45 -0500

    Add validation to Specialty

--- app/models/specialty.rb
+++ app/models/specialty.rb
@@ -1,2 +1,4 @@
 class Specialty < ActiveRecord::Base
+  validates :name, :presence => { :message => "es requerido!"  }
+  validates :name, :uniqueness => { :message => "ya ha sido agregado!"  }
 end
