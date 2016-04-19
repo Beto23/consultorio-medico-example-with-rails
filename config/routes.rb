c427b455e6df49800cd737ff63d8a8fd839c3107
Beto23 <josee-45@hotmail.com>
2016-04-18 21:32:05 -0500

    Add migration devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :specialties#, only: [:create, :destroy, :update, :index]
 
   # The priority is based upon order of creation: first created -> highest priority.
