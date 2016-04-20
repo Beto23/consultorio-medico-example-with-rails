ffab3ba22219f77c44177aa196bcb9358ba5c34b
Beto23 <josee-45@hotmail.com>
2016-04-20 16:21:25 -0500

    Add custom fields to devise user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160420072424) do
+ActiveRecord::Schema.define(version: 20160420211656) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 20160420072424) do
     t.integer  "permission_level"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
