d07e40df8c6c062acff1c23c2401b6b8a4a36119
Beto23 <josee-45@hotmail.com>
2016-04-22 00:23:42 -0500

    add relation 1:n table Medics

--- app/models/specialty.rb
+++ app/models/specialty.rb
@@ -1,4 +1,5 @@
 class Specialty < ActiveRecord::Base
   validates :name, :presence => { :message => "es requerido!"  }
   validates :name, :uniqueness => { :message => "ya ha sido agregado!"  }
+  has_many :medics
 end
