1078e8d9850e9de0f2bb703e8514851f773fdeb2
Beto23 <josee-45@hotmail.com>
2016-04-23 01:57:23 -0500

    Add to Delete a medic

--- app/controllers/medics_controller.rb
+++ app/controllers/medics_controller.rb
@@ -58,7 +58,7 @@ class MedicsController < ApplicationController
   def destroy
     @medic.destroy
     respond_to do |format|
-      format.html { redirect_to medics_url, notice: 'Medic was successfully destroyed.' }
+      format.html { redirect_to medics_path, notice: 'Medic was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
