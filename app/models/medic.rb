c1a4a0b509090cd464d214e537c6d1e270d0897a
Beto23 <josee-45@hotmail.com>
2016-04-23 00:14:02 -0500

    Add validation to formMedic

--- app/models/medic.rb
+++ app/models/medic.rb
@@ -2,5 +2,7 @@ class Medic < ActiveRecord::Base
   belongs_to :doctor
   belongs_to :specialty
   validates :doctor_id, :uniqueness => { :message => "ya ha sido agregado!"  }
-
+  validates :phone, presence: true
+  validates :address, presence: true
+  validates :specialty_id, presence: true
 end
