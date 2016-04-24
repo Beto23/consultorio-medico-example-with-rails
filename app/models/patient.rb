1009eb37b2a74523e04e6edfef58aa3fa901b759
Beto23 <josee-45@hotmail.com>
2016-04-23 21:17:50 -0500

    Add validation patients

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,11 @@
 class Patient < ActiveRecord::Base
+  validates :first_name, presence: true, length: { maximum: 20  }
+  validates :last_name, presence: true, length: { maximum: 20  }
+  validates :gender, presence: true
+  validates :state, presence: true, length: { maximum: 20  }
+  validates :city, presence: true, length: { maximum: 20  }
+  validates :address, presence: true, length: { maximum: 20  }
+  validates :cp, presence: true, length: { minimum: 5  }, length: { maximum: 5  }
+  validates :email, presence: true, length: { maximum: 50  }
+  validates :phone, presence: true, length: { maximum: 14  }
 end
