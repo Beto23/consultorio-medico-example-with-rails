a84ed75cc972ba3a04d2a56c3a0a867a271d395e
Beto23 <josee-45@hotmail.com>
2016-04-24 15:49:48 -0500

    Validate form consultation

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -1,4 +1,8 @@
 class Consultation < ActiveRecord::Base
   belongs_to :medic
   belongs_to :patient
+  validates :patient_id, presence: true
+  validates :medic_id, presence: true
+  validates :time, presence: true, length: { maximum: 5  }
+  validates :fecha, presence: true
 end
